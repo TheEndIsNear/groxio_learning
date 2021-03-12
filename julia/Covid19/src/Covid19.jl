50d7a47dfe31bbb63f9778bc14805b3b07af5cb7
Matt Willy <matt.willy@gmail.com>
2021-03-11 20:15:40 -0700

    display data from covid counts

--- julia/Covid19/src/Covid19.jl
+++ julia/Covid19/src/Covid19.jl
@@ -1,5 +1,13 @@
 module Covid19
+  using CSV, DataFrames, Query
+  export data
 
-greet() = print("Hello World!")
+  function data()
+    raw = CSV.read("data/full_data.csv", DataFrame)
+    cases = coalesce.(raw, 0.0)
 
+    population = CSV.read("data/locations.csv", DataFrame)
+
+    innerjoin(cases, population, on = :location)
+  end
 end # module
